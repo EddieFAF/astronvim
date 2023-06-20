609a1fe344a9e8106832b39e877971912b424be8
EddieFAF <eddie.faf@googlemail.com>
2023-06-20 10:16:35 +0200

    cmp changed

--- mappings.lua
+++ mappings.lua
@@ -11,7 +11,8 @@ return {
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
     ["<leader>bD"] = {
       function()
-        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(bufnr) end)
+        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(
+          bufnr) end)
       end,
       desc = "Pick to close",
     },
@@ -20,6 +21,8 @@ return {
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    ["S-h"] = { "<cmd>:bprevious<cr>", desc = "previous buffer" },
+    ["S-l"] = { "<cmd>:bnext<cr>", desc = "next buffer" },
   },
   t = {
     -- setting a mapping to false will disable it
--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,44 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    config = function()
+      require("catppuccin").setup {}
+    end,
+  },
+  { -- override nvim-cmp plugin
+    "hrsh7th/nvim-cmp",
+    keys = { ":", "/", "?" }, -- lazy load cmp on more keys along with insert mode
+    dependencies = {
+      "hrsh7th/cmp-cmdline", -- add cmp-cmdline as dependency of cmp
+    },
+    config = function(plugin, opts)
+      local cmp = require "cmp"
+      -- run cmp setup
+      cmp.setup(opts)
+
+      -- configure `cmp-cmdline` as described in their repo: https://github.com/hrsh7th/cmp-cmdline#setup
+      cmp.setup.cmdline("/", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = "buffer" },
+        },
+      })
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = "path" },
+        }, {
+          {
+            name = "cmdline",
+            option = {
+              ignore_cmds = { "Man", "!" },
+            },
+          },
+        }),
+      })
+    end,
+  },
 }
