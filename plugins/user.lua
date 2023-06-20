e8b88901748163bee67f30a4f78282ec6b478952
EddieFAF <eddie.faf@googlemail.com>
2023-06-20 12:02:38 +0200

    catppuccin added

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,18 +9,11 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    config = function()
-      require("catppuccin").setup {}
-    end,
-  },
-  { -- override nvim-cmp plugin
+  {                           -- override nvim-cmp plugin
     "hrsh7th/nvim-cmp",
     keys = { ":", "/", "?" }, -- lazy load cmp on more keys along with insert mode
     dependencies = {
-      "hrsh7th/cmp-cmdline", -- add cmp-cmdline as dependency of cmp
+      "hrsh7th/cmp-cmdline",  -- add cmp-cmdline as dependency of cmp
     },
     config = function(plugin, opts)
       local cmp = require "cmp"
@@ -49,4 +42,29 @@ return {
       })
     end,
   },
+  {
+    "rebelot/heirline.nvim",
+    opts = function(_, opts)
+      local status = require("astronvim.utils.status")
+      opts.statusline = {                                                            -- statusline
+        hl = { fg = "fg", bg = "bg" },
+        status.component.mode { mode_text = { padding = { left = 1, right = 1 } } }, -- add the mode text
+        status.component.git_branch(),
+        status.component.file_info { filetype = {}, filename = false, file_modified = false },
+        status.component.git_diff(),
+        status.component.diagnostics(),
+        status.component.fill(),
+        status.component.cmd_info(),
+        status.component.fill(),
+        status.component.lsp(),
+        status.component.treesitter(),
+        status.component.nav(),
+        -- remove the 2nd mode indicator on the right
+      }
+
+      -- return the final configuration table
+      return opts
+    end,
+  },
+
 }
