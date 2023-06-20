5c3447582ecd086fa412aba2cbb0f30dc59c3c6b
EddieFAF <eddie.faf@googlemail.com>
2023-06-20 18:12:52 +0200

    community packages added

--- plugins/community.lua
+++ plugins/community.lua
@@ -5,5 +5,15 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.onedarkpro-nvim" },
+  { import = "astrocommunity.colorscheme.everforest" },
+  { import = "astrocommunity.colorscheme.mini-base16" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.comment.mini-comment" },
+  { import = "astrocommunity.editing-support.nvim-ts-rainbow2"},
+  { import = "astrocommunity.indent.mini-indentscope"},
+  { import = "astrocommunity.motion.mini-ai"},
+  { import = "astrocommunity.motion.mini-bracketed"},
+  { import = "astrocommunity.motion.mini-move"},
+  { import = "astrocommunity.motion.mini-surround"},
 }
